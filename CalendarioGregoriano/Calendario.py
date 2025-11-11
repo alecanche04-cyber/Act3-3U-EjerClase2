aa0f45ccf848f42b691f31c433c7ab340b1e9abe
Alan-2023Dts <150711078+Alan-2023Dts@users.noreply.github.com>
2025-11-11 12:32:36 -0600

    Función que calcula el numero del día
    Hacemos una lista
    1.Domingo
    2.Lunes
    3.Martes
    4.Miercoles
    5.Jueves
    6.Viernes
    7.Sabado
    Tomando esta lista en cuenta vamos a retornar un numero del 1 al 7 Para asignar un nombre del diccionario
    Tomamos la formula de zeller
    Donde:
    - h = día de la semana
    - q = día del mes
    - m = mes
    - K = año del siglo (es decir, )
    - J = siglo
    Terminamos ajustando los numero para que 0 sea el valor de domingo en el diccionario
    
    Retornamos el valor del día para el diccionario

--- CalendarioGregoriano/Calendario.py
+++ CalendarioGregoriano/Calendario.py
@@ -5,5 +5,17 @@ def es_bisiesto(año):
         return True
     else:
         return False
-    
-    
+def dia_1_enero(año):
+    # Algoritmo de Zeller modificado para calcular el día de la semana del 1 de enero
+    q = 1  # Día del mes
+    m = 13  # Enero es tratado como el mes 13 del año anterior
+    K = (año - 1) % 100  # El año dentro del siglo
+    J = (año - 1) // 100  # El siglo
+
+    # Fórmula de Zeller
+    f = q + ((13 * (m + 1)) // 5) + K + (K // 4) + (J // 4) - (2 * J)
+    dia_semana = f % 7 #Sacamos el modulo que va a indicar el dia de la semana
+    # Ajustar para que 0 = Domingo, 1 = Lunes, etc...
+    dia_semana = (dia_semana + 7) % 7
+
+    return dia_semana
