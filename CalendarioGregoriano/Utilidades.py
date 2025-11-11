1488bdf3db316ac0b830c9b57f86f6c430547468
alecanche04-cyber <alecanche04@gmail.com>
2025-11-11 12:05:54 -0600

    Leer año
    En caso que el año sea invalido se le pide al usuario volver a ingresar un año

--- CalendarioGregoriano/Utilidades.py
+++ CalendarioGregoriano/Utilidades.py
@@ -14,4 +14,16 @@ def nombre_dia(numero): # Devuelve el nombre del día de la semana dado su núme
 def año_valido(año): # Verifica si un año es válido (mayor que 1582)
     return año > 1582 # El calendario gregoriano comenzó en 1582
 
+def leer_año(): # Solicita al usuario que ingrese un año válido
+    while True:
+        try:
+            año = int(input("Ingrese un año (mayor que 1582): "))
+            if año_valido(año):
+                return año
+            else:
+                print("Año inválido. Por favor, ingrese un año mayor que 1582.")
+        except ValueError:
+            print("Entrada inválida. Por favor, ingrese un número entero.")
+
+
 
