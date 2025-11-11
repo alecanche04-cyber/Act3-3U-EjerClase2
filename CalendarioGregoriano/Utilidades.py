15f9664cd806cdbf817f7c01eacf3112485aa9c8
alecanche04-cyber <alecanche04@gmail.com>
2025-11-11 11:59:22 -0600

    Conversion de numero al nombre del dia
    Devuelve el nombre del día de la semana dado su número (1-7)

---
+++ CalendarioGregoriano/Utilidades.py
@@ -0,0 +1,12 @@
+def nombre_dia(numero): # Devuelve el nombre del día de la semana dado su número (1-7)
+    
+    dias = {
+        1: "Lunes",
+        2: "Martes",
+        3: "Miércoles",
+        4: "Jueves",
+        5: "Viernes",
+        6: "Sábado",
+        7: "Domingo"
+    }
+    return dias.get(numero, "Número de día inválido") # Retorna un mensaje si el número no es válido
